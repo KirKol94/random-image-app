40a4e7aaa4cfc978b4968d83749dfbdd3d463581
Kirill Kolchanov <kirkol94@ya.ru>
2022-10-13 10:56:54 +0500

    likes page map array

--- src/pages/likesPage/LikesPage.jsx
+++ src/pages/likesPage/LikesPage.jsx
@@ -1,8 +1,19 @@
+import { useSelector } from 'react-redux';
+import { likesSelector } from '../../store/selectors';
+
 export default function LikesPage() {
+  const likes = useSelector(likesSelector);
+  console.log(likes);
+
   return (
     <main>
       <section className="container mx-auto">
         <h1 className="mt-3 text-2xl">Like images</h1>
+        <div className="flex flex-col space-y-2">
+          {likes?.map(({ url, keyword, id }) => (
+            <img key={id} src={url} alt={keyword} className="w-full" />
+          ))}
+        </div>
       </section>
     </main>
   );
