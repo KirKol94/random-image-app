e7595e71697ca5e031d13f1d55448930bacbc8f2
Kirill Kolchanov <kirkol94@ya.ru>
2022-10-14 12:35:43 +0500

    если фаворит лист пуст - отображается предупреждение, что ничего нет

--- src/pages/likesPage/LikesPage.jsx
+++ src/pages/likesPage/LikesPage.jsx
@@ -9,12 +9,12 @@ export default function LikesPage() {
     <main>
       <section className="container mx-auto">
         <div className="my-3 grid grid-cols-3 gap-3 items-center">
+          {!likes.length && <h1 className="text-2xl">Page is empty :(</h1>}
           {likes?.map(({ url, keyword, id }) => (
             <Link to={`/likes/${id}`} key={id}>
               <img
                 src={url}
                 alt={keyword}
-                className="w-full"
               />
             </Link>
           ))}
